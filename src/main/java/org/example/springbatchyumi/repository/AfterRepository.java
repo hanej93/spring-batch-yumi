d1ab6ee49aa05afb0150c96115254ab2ae237c6d
EJHan <hanej93@naver.com>
2024-12-06 22:54:39 +0900

    배치 처리1 : 테이블 to 테이블

---
+++ src/main/java/org/example/springbatchyumi/repository/AfterRepository.java
@@ -0,0 +1,7 @@
+package org.example.springbatchyumi.repository;
+
+import org.example.springbatchyumi.entity.AfterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AfterRepository extends JpaRepository<AfterEntity, Long> {
+}
---
+++ src/main/java/org/example/springbatchyumi/repository/BeforeRepository.java
@@ -0,0 +1,7 @@
+package org.example.springbatchyumi.repository;
+
+import org.example.springbatchyumi.entity.BeforeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BeforeRepository extends JpaRepository<BeforeEntity, Long> {
+}
