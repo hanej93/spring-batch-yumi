50f47f0780d348c2e5c8a7e0fa22e8436235ff6e
EJHan <hanej93@naver.com>
2024-12-06 21:49:00 +0900

    :tada: Initial commit

---
+++ src/main/java/org/example/springbatchyumi/SpringBatchYumiApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbatchyumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchYumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchYumiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/springbatchyumi/SpringBatchYumiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbatchyumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchYumiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
