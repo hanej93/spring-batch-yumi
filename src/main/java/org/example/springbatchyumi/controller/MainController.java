a12269747ed42061b18edb96792d75f94e56b297
EJHan <hanej93@naver.com>
2024-12-06 23:07:38 +0900

    배치 처리1 : 실행 및 스케쥴

---
+++ src/main/java/org/example/springbatchyumi/controller/MainController.java
@@ -0,0 +1,30 @@
+package org.example.springbatchyumi.controller;
+
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	private final JobLauncher jobLauncher;
+	private final JobRegistry jobRegistry;
+
+	public MainController(JobLauncher jobLauncher, JobRegistry jobRegistry) {
+		this.jobLauncher = jobLauncher;
+		this.jobRegistry = jobRegistry;
+	}
+
+	@GetMapping("/first")
+	public String firstApi(@RequestParam("value") String value) throws Exception {
+		JobParameters jobParameters = new JobParametersBuilder()
+			.addString("date", value)
+			.toJobParameters();
+		jobLauncher.run(jobRegistry.getJob("firstJob"), jobParameters);
+		return "ok";
+	}
+}
